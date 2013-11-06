859d1dbcec067c294fca289cb3ce7b08c1624cab
S Smith <ssmith1975.soc@gmail.com>
2013-11-06 08:11:33 -0600

    First Commit - Unit Test in C#
    Omitted NUnit.2.6.2 package

---
+++ unit-test/Bank/BankTest/UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BankAccountNS;
+
+namespace BankTest
+{
+    [TestClass]
+    public class BankAccountTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    } 
+}
